64a950af46958d69d0cf1051677f1d0bec854579
xxxjahlilallenxxx <jahlilallen@gmail.com>
2022-04-01 14:39:46 -0400

    Created Music.Me Repository

---
+++ Backend/p2-music-me/src/main/java/com/revature/p2musicme/P2MusicMeApplication.java
@@ -0,0 +1,13 @@
+package com.revature.p2musicme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P2MusicMeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(P2MusicMeApplication.class, args);
+	}
+
+}
