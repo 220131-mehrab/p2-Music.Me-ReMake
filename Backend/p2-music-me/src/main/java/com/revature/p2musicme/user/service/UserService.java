1a5d9481a29db1ea5488b8e1e78e394606856419
xxxjahlilallenxxx <jahlilallen@gmail.com>
2022-04-01 21:56:49 -0400

    Updated 3 files

--- Backend/p2-music-me/src/main/java/com/revature/p2musicme/user/service/UserService.java
+++ Backend/p2-music-me/src/main/java/com/revature/p2musicme/user/service/UserService.java
@@ -2,6 +2,13 @@ package com.revature.p2musicme.user.service;
 
 import com.revature.p2musicme.user.entity.User;
 
+import java.util.Collection;
+import java.util.UUID;
+
 public interface UserService {
     User create(User user);
+    Collection<User> list(int limit);
+    User get(UUID id);
+    User update(User user);
+    Boolean delete(UUID id);
 }
